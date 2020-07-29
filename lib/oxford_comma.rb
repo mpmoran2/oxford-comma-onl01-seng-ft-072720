d06709b59cef101ac295daea82eb06580fb2e7da
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-29 20:47:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   return array.join(' and ') if array.size < 3
   array[-1] = "and " + array[-1]
   array.join(', ')
+=======
+  array = ["kiwi"].join
+end
+  array = ["kiwi","durian"].join(" and ")
+>>>>>>> c772ffaf7fd94e5e64a64cc6331d3797dfb99da8
 end
   
 
